db1a55535b05e7f78197ec0112f1c0b1aef913f9
Alicia <alicia.ibanez.escudero@gmail.com>
2025-03-12 09:35:53 +0100

    Commit 2 con métodos

--- Editorial.java
+++ Editorial.java
@@ -7,4 +7,23 @@ public class Editorial {
         nombre = "Luis";
         pais = "España";
     }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+ 
+    public String getNombre() {
+        return nombre;
+    }
+    public String getPais(){
+        return pais;
+    }
+    public void setPais(String elPais){
+        elPais = pais;
+    }
+ 
+    public void imprimirDetalles() {
+        System.out.println ("Nombre: " + nombre + " " + "País: " + pais);
+    }
+ 
 }
